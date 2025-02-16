f02cef520ed13f40d0294a4b906387826739552e
BiditG <biditgiri123@gmail.com>
2025-02-16 22:09:50 +0545

    Made the frontend for login and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+import { Google as GoogleIcon } from '@mui/icons-material';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <Container maxWidth="xs" sx={{ mt: 8 }}>
+      <Typography variant="h4" component="h1" gutterBottom align="center">
+        Login
+      </Typography>
+      <Box component="form" sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Button
+          fullWidth
+          variant="outlined"
+          startIcon={<GoogleIcon />}
+          sx={{ mb: 2 }}
+        >
+          Sign in with Google
+        </Button>
+        <Button
+          fullWidth
+          variant="text"
+          sx={{ textTransform: 'none' }}
+        >
+          Forgot Password?
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+import { Google as GoogleIcon } from '@mui/icons-material';
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <Container maxWidth="xs" sx={{ mt: 8 }}>
+      <Typography variant="h4" component="h1" gutterBottom align="center">
+        Register
+      </Typography>
+      <Box component="form" sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="new-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Register
+        </Button>
+        <Button
+          fullWidth
+          variant="outlined"
+          startIcon={<GoogleIcon />}
+          sx={{ mb: 2 }}
+        >
+          Register with Google
+        </Button>
+        <Button
+          fullWidth
+          variant="text"
+          sx={{ textTransform: 'none' }}
+        >
+          Forgot Password?
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
-export default Register
+export default Register;
